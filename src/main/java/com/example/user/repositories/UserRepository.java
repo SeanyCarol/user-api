a61a69b40a95e367f4b70f7702446ba6a24c3059
SeanyCarol <seany@ice.ufjf.br>
2021-09-05 21:41:59 -0300

    Method to search users by salary developed

--- src/main/java/com/example/user/repositories/UserRepository.java
+++ src/main/java/com/example/user/repositories/UserRepository.java
@@ -2,8 +2,13 @@ package com.example.user.repositories;
 
 import com.example.user.models.User;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  
+
+  @Query("SELECT obj FROM User obj WHERE obj.salary >= :minSalary AND obj.salary <= :maxSalary")
+  Page<User> searchBySalary(Double minSalary, Double maxSalary, Pageable pageable); 
 }
