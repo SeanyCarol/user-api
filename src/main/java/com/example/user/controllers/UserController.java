01f7c2d7f831874b5ba86b54d31cb5ef33102092
SeanyCarol <seany@ice.ufjf.br>
2021-09-05 21:36:40 -0300

    Method to paged users developed

--- src/main/java/com/example/user/controllers/UserController.java
+++ src/main/java/com/example/user/controllers/UserController.java
@@ -6,6 +6,8 @@ import com.example.user.models.User;
 import com.example.user.repositories.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,12 @@ public class UserController {
   @GetMapping
   public ResponseEntity<List<User>> findAll() {
     List<User> result = repository.findAll();
+    return ResponseEntity.ok(result);
+  }
+
+  @GetMapping(value = "/page")
+  public ResponseEntity<Page<User>> findAll(Pageable pageable) {
+    Page<User> result = repository.findAll(pageable);
     return ResponseEntity.ok(result);
   }
 }
